bf21c0eb30565a64df547e7caeb6b5d64a8a3689
GitHub Action - Deploy <dyego.maas@gmail.com>
2022-02-18 20:11:47 +0000

    Atualização

---
+++ js/site-embedded-podcasts-tech.js
@@ -0,0 +1,28 @@
+function createIframeElement(episodeId) {
+    let iframe = document.createElement('iframe')
+    iframe.setAttribute('style', 'border-radius:12px')
+    iframe.setAttribute('width', '100%')
+    iframe.setAttribute('height', '232')
+    iframe.setAttribute('frameBorder', '0')
+    iframe.setAttribute('allowfullscreen', '')
+    iframe.setAttribute('allow', 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture')
+    iframe.setAttribute('src', `https://open.spotify.com/embed/episode/${episodeId}`)
+    return iframe
+}
+
+function fillWith(containerId, episodeIds) {
+    episodeIds
+        .reverse()
+        .forEach((episodeId, index) => {
+            let container = document.getElementById(containerId)
+            let iframe = createIframeElement(episodeId)
+            let paragraph = document.createElement('p')
+            paragraph.appendChild(iframe)
+            container.appendChild(paragraph)
+        });
+}
+
+fillWith('podcasts-tech', [
+    '6WmjaeH6WWM26dyuvvZXsd',
+    '1DQejs2AVZ7cRdpkkZpH9u'
+])
